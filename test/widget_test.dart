feebf061d9d558da98217ec5305996e069652402
mavy55 <mavy55@atomicmail.io>
2026-07-15 00:03:52 +0330

    fix: correct package import in widget_test from 'v2ray' to 'flaming_cherubim'

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:v2ray/main.dart';
+import 'package:flaming_cherubim/main.dart';
 
 void main() {
   testWidgets('App initialization smoke test', (WidgetTester tester) async {
